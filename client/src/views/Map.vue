3bba3704ca681825b1894d1d1a00b48b7395f0d9
softdrum <drewzerg@gmail.com>
2020-05-30 15:31:54 +0300

    removed leaflet map

--- client/src/views/Map.vue
+++ client/src/views/Map.vue
@@ -5,42 +5,7 @@
 </template>
 
 <script>
-// '../maps/SaintPetersburg.mbtiles'
-import L from 'leaflet'
-export default {
-  name: "Example",
-  components: {
-  },
-  data() {
-    return {
-      map: null,
-      tileLayer: null,
-      layers: [],
-    };
-  },
-  mounted() {
-    this.initMap()
-  },
-  methods: {
-    initLayers() {},
-    initMap() {
-      this.map = L.map(this.$refs.map).setView([59.925783, 30.331176], 20);
-      // this.tileLayer = L.tileLayer(
-      //   'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
-      //   {
-      //     maxZoom: 18,
-      //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
-      //   }
-      // );
-      this.tileLayer = L.tileLayer.mbTiles(' ', {
-        minZoom: 0,
-        maxZoom: 6
-      });
-
-      this.tileLayer.addTo(this.map);
-    },
-  }
-};
+export default {}
 </script>
 <style scoped>
   .map {
