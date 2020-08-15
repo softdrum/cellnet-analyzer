7eb3b541ae43da3f8a4495e30ef37c1282936632
softdrum <drewzerg@gmail.com>
2020-08-16 02:35:55 +0300

    Removed sqlite models

--- server/src/modem/index.js
+++ server/src/modem/index.js
@@ -1,4 +1,8 @@
 const Modem = require('./modem')
-module.exports = (port, baudRate, debug) => {
+const MockModem = require('./mockModem')
+
+
+module.exports = (port, baudRate, debug, test=true) => {
+  if (test) return new MockModem(port, baudRate, debug)
   return new Modem(port, baudRate, debug)
 }
