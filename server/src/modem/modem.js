7f33b6e5c6c7f91d0ee517e4b3ad6824c6602b4f
softdrum <drewzerg@gmail.com>
2020-08-16 02:38:31 +0300

    Refactored modem class and helpers

--- server/src/modem/modem.js
+++ server/src/modem/modem.js
@@ -1,9 +1,6 @@
 const serialPortGSM = require('serialport-gsm')
 const modemOptions = require('./modemOptions')
-const modemRegExp = require('./modemRegexp')
-const modemHelpers = require('./modemHelpers')
 
-function sleep (ms) {return new Promise(resolve => {setTimeout(() => resolve(), ms)})}
 
 class Modem{
   constructor(port, baudRate, logger=null) {
@@ -12,33 +9,11 @@ class Modem{
     this.modem = serialPortGSM.Modem()
     this.connected = false
     this.isBusy = false
-    this.logMode = false
   }
-  setModemConnectionStatus (value) {
-    this.connected = value
-  }
-  setBusyMode (value) {
-    this.isBusy = value
-  }
-  setLogMode (value) {
-    console.log('setting log mode');
-    this.logMode = value
-  }
-  getModemConnectionStatus () {
-    return this.connected
-  }
-  getBusyMode () {
-    return this.isBusy
-  }
-  getLogMode () {
-    return this.logMode
-  }
-  
   initModem() {
     return this.modem.open(this.port, this.options)
       .then(result => {
         console.log(result);
-        console.log('Modem connection is opened');
         this.setModemConnectionStatus(true)
         return this.getModemConnectionStatus()
       })
@@ -55,77 +30,19 @@ class Modem{
         .then( result => {
           resolve(result)
         })
-      
     }) 
   }
-  getSignalQuality () {
-    return this.executeAtCommand('AT+CSQ')
-    .then( (result) => {
-      if (result.status === 'ERROR') throw 'Error: Can not get signal quality'
-      let data = result.data.result.match(modemRegExp.signalQuality)
-      if (!data) throw 'Error: Can not get signal quality'
-      return {
-        s_lvl: modemHelpers.calculateSignalLevel(data.groups.s_lvl),
-        ber: modemHelpers.calculateBitErrorRate(data.groups.ber)
-      }
-    })
+  setModemConnectionStatus (value) {
+    this.connected = value
   }
-  getGPSCoordinates () {
-    return this.executeAtCommand('AT+CGPSINFO')
-    .then( (result) => {
-      if (result.status === 'ERROR') throw 'Error: Can not get signal quality'
-      let data = result.data.result.match(modemRegExp.signalQuality)
-      if (!data) throw 'Error: Can not get signal quality'
-      return {
-        s_lvl: modemHelpers.calculateSignalLevel(data.groups.s_lvl),
-        ber: modemHelpers.calculateBitErrorRate(data.groups.ber)
-      }
-    })
+  setBusyMode (value) {
+    this.isBusy = value
   }
-  getAvailableOperators () {
-    return this.executeAtCommand('AT+COPS=?')
-    .then(response => {
-      if (response.status === 'ERROR') throw 'Error: Can not get available operators'
-      let data = response.data.result.matchAll(modemRegExp.availableOperators)
-      if (!data) throw 'Error: Can not get available operators'
-      const available_operators = [...data].map(item => { return item.groups })
-      const unique_operators = modemHelpers.filterUniqueOperators(available_operators)
-      return {
-        topic: 'available_operators',
-        data:  unique_operators
-      }
-    })
+  getModemConnectionStatus () {
+    return this.connected
   }
-  getBasestationInfo () {
-    return this.executeAtCommand('AT+CPSI?')
-      .then(response => {
-        if (response.status === 'ERROR') throw 'Can not get BS Info'
-        if (response.data.result.trim() === 'NO SERVICE,Online') throw 'No service'
-        const info = modemHelpers.extractBasestationInfo(response.data.result)
-        if (!info) throw `Unknown data: ${response}`
-        return info.groups
-      })
-  }
-  setPrefferedSystemMode(mode) {
-    return this.executeAtCommand(`AT+CNMP=${modemHelpers.getSystemModeCode(mode)}`)
-  }
-  changeNetMode (mode) {
-    return this.executeAtCommand(`AT+CNMP=${modemHelpers.getSystemModeCode(mode)}`)
-    .then (response => {
-      if (response.data.result === 'ERROR')  throw 'Unable to set preffered system mode'
-      return sleep(5000).then(() => {
-        return this.getBasestationInfo()
-      })
-    })
-    .then (response => {
-      this.modemBusy = false
-      if (response.mode === mode || mode === 'auto') return 'SUCCESS'
-      else throw 'Preffered mode was changed but modem is not connected to suitable base station'
-    })
-    .catch (error => {
-      this.modemBusy = false
-      throw error
-    })
+  getBusyMode () {
+    return this.isBusy
   }
 }
 
--- server/src/utils/geo.js
+++ server/src/utils/geo.js
@@ -1,20 +1,23 @@
 function deg2rad(deg) {
   return deg * (Math.PI/180)
 }
-module.exports = {
-  getDistanceFromLatLonInKm(oldLatLong, newLatLong) {
-    
-    const R = 6371; // Radius of the earth in km
-    const dLat = deg2rad(newLatLong.lat-oldLatLong.lat);  // deg2rad below
-    const dLon = deg2rad(newLatLong.lng-oldLatLong.lng); 
+function getDistanceFromLatLonInKm(oldLatLong, newLatLong) {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(newLatLong.lat-oldLatLong.lat);  // deg2rad below
+  const dLon = deg2rad(newLatLong.lng-oldLatLong.lng); 
 
-    let a = 
-      Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(deg2rad(oldLatLong.lat)) * Math.cos(deg2rad(newLatLong.lat)) * 
-      Math.sin(dLon/2) * Math.sin(dLon/2)
-      ; 
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    const d = R * c; // Distance in km
-    return d;
-  },
+  let a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(oldLatLong.lat)) * Math.cos(deg2rad(newLatLong.lat)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  const d = R * c; // Distance in km
+  return d;
+}
+module.exports = {
+  filterFeatures (features, coordinates, radius) {
+    return features
+      .filter(elem => getDistanceFromLatLonInKm(coordinates, {lat: elem.geometry.coordinates[1], lng: elem.geometry.coordinates[0]}) <= radius)
+  }
 }
