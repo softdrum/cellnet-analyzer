c5b3ecda8a8e0b948636ab13bde6cca1bea987ba
softdrum <drewzerg@gmail.com>
2020-08-16 02:34:16 +0300

    Removed auth controller and route

--- server/src/router/auth.routing.js
+++
@@ -1,6 +0,0 @@
-const authController = require('../controllers/auth.controller')
-
-module.exports = (app) => {
-  app.post('/api/register', authController.register)
-  app.post('/api/login', authController.login)
-}
