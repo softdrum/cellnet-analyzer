e5df5790cdc5155956f0d1e30407b63184883694
softdrum <drewzerg@gmail.com>
2020-08-26 23:06:19 +0300

    Refactored modem class, some funcs moved in modem.service

--- server/src/modem/index.js
+++
@@ -1,8 +0,0 @@
-const Modem = require('./modem')
-const MockModem = require('./mockModem')
-
-
-module.exports = (port, baudRate, debug, test=true) => {
-  if (test) return new MockModem(port, baudRate, debug)
-  return new Modem(port, baudRate, debug)
-}
