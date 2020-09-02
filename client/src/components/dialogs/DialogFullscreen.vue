4ec95822db16d117f961bdd29076036eaf2e1b6d
softdrum <drewzerg@gmail.com>
2020-09-03 01:36:10 +0300

    Added fulscreen dialog

---
+++ client/src/components/dialogs/DialogFullscreen.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-dialog persistent v-model="value" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <v-btn @click="onClick">close</v-btn>
+      <slot />
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DialogFullscreen',
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    onClick () {
+      this.$emit('close')
+      this.$emit('input', false)
+    }
+  }
+}
+</script>
