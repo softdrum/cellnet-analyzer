79534d934e84d50cbaba4bb7e0f7582455cb8a27
softdrum <drewzerg@gmail.com>
2020-09-16 23:33:16 +0100

    db-api added static routes

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 8082,
+    port: process.env.PORT || 8081,
     db: {
         addr: process.env.MONGO_DB_ADDR || 'localhost',
         port: process.env.MONGO_DB_PORT || 27017,
