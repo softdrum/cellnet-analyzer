0cad435b9d500e28630c7facd1e091f56ae147e1
softdrum <drewzerg@gmail.com>
2020-09-24 00:10:49 +0300

    Refactor url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,6 +3,6 @@ const apiURL = process.env.VUE_APP_DB_API_URL || `http://localhost:8081/api`;
 
 export default () => {
     return axios.create({
-        baseURL: apiURL
+        baseURL: apiURL+'/api'
     }) 
 }
