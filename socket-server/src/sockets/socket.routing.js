91024c11eb0b8a312d4058be674a1b71b424d007
softdrum <drewzerg@gmail.com>
2020-09-23 19:09:36 +0100

    Renamed modem controller to socket controller

--- socket-server/src/sockets/socket.routing.js
+++ socket-server/src/sockets/socket.routing.js
@@ -1,9 +1,9 @@
 module.exports = (socket, modem) => {
-  const modemController = require('../controllers/modem.controller')(modem)
+  const socketController = require('../controllers/socket.controller')(modem)
 
-  socket.on('changeMode', modemController.changeNetworkMode);
-  socket.on('getSignalQuality', modemController.getSignalQuality);
-  socket.on('getAvailableOperators', modemController.getAvailableOperators);
-  socket.on('getGeoLocation', modemController.getGeoLocation);
-  socket.on('getMeasureData', modemController.getMeasureData);
+  socket.on('changeMode', socketController.changeNetworkMode);
+  socket.on('getSignalQuality', socketController.getSignalQuality);
+  socket.on('getAvailableOperators', socketController.getAvailableOperators);
+  socket.on('getGeoLocation', socketController.getGeoLocation);
+  socket.on('getMeasureData', socketController.getMeasureData);
 }
