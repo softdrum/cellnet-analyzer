38207cf7a37be4aa2243d78d6822012faf48fcd7
softdrum <drewzerg@gmail.com>
2020-09-27 15:20:17 +0300

    Add multi geojson file getter

--- server/src/controllers/basestation.controller.js
+++ server/src/controllers/basestation.controller.js
@@ -6,11 +6,11 @@ module.exports = {
      * Extracts all basestations within a radius of one km
      */
     try {
-      const coordinates = {lng: req.query.lng, lat: req.query.lat}
-      const radius = parseInt(req.query.radius)
-      const basestations = await basestationService.getBasestationsAround(coordinates, radius)
+      const coordinates = {lng: req.query.lng, lat: req.query.lat};
+      const radius = parseInt(req.query.radius);
+      const basestations = await basestationService.getBasestationsAround(coordinates, radius);
       console.log('sending bs', basestations.length);
-      res.json(basestations)
+      res.json(basestations);
     } catch (error) {
       console.log(error);
       res.status(400).send(error)
--- server/src/services/basestation.service.js
+++ server/src/services/basestation.service.js
@@ -6,16 +6,22 @@ const cacheDuration = 300
 const fs = require('fs').promises
 const path = require("path");
 const appDir = path.dirname(require.main.filename);
+
 module.exports = {
   async getBasestationsAround (coordinates, radius) {
       let basestations
       console.log('getting data');
+      if ((lat >= 55.123 && lat <= 56.2) && (lng >= 36.8 && lng <= 38.43)) {
+        city = 'moscow'
+      } else {
+        'saint_petersburg'
+      }
       const cachedData = mcache.get('basestations')
       if (cachedData) {
         basestations = cachedData
       }
       else {
-        let basestationsJSON = await fs.readFile(`${appDir}/src/geojson/bs_import.json`);
+        let basestationsJSON = await fs.readFile(`${appDir}/src/geojson/${city}.json`);
         let basestationsArr = JSON.parse(basestationsJSON);
         if (basestationsArr) {
           basestations = basestationsArr
